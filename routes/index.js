4b91e4a22a0f4b725f87b152ca38ca81aa9031e7
deadalter <deadalter@yandex.ru>
2017-04-12 00:39:52 +0300

    Fix index page (add input, button and etc.), fix css

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+//global.current_user;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Автосервис' });
 });
 
 module.exports = router;
